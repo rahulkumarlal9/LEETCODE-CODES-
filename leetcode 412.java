3804f25e39519d1bf84a71979eb488261733741b
Rahul kumar lal <127595973+rahulkumarlal9@users.noreply.github.com>
2024-08-31 12:39:22 +0530

    Create leetcode 412.java

---
+++ leetcode 412.java
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/fizz-buzz/
+
+
+import java.util.ArrayList;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 1; i <=n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                arr.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                arr.add("Fizz");
+            } else if (i % 5 == 0) {
+                arr.add("Buzz");
+            } else {
+                arr.add(Integer.toString(i));
+            }
+        }
+        return arr;
+    }
+}
